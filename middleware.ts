e88f16db5a033d0513093ea1ddd3194279320f2e
Advaittsingh <advaitsingh05@gmail.com>
2026-01-22 21:52:25 +0530

    Fix middleware: set runtime to nodejs to resolve __dirname error

--- middleware.ts
+++ middleware.ts
@@ -6,14 +6,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: '/:path*',
+  runtime: 'nodejs', // 👈 THIS IS THE FIX
 }
